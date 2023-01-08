15c171dedece1b29dad2b1ed35caf3f67618cef5
Unzila Mehtab <rizviunzila0786@gmail.com>
2023-01-08 19:43:25 +0530

    first commit

---
+++ 1_LinearSearch.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define size 100
+using namespace std;
+
+int main()
+{
+    int a[size], n;
+    cout<<"Enter the number of elements: "<<endl;
+    cin>>n;
+
+    cout<<"Enter the elements in the array: "<<endl;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    int num, flag=0;
+    cout<<"Enter the element you want to search: ";
+    cin>>num;
+
+    for(int i=0;i<n;i++){
+        if(a[i]==num)
+            flag=1;
+    }
+    if(flag==1) cout<<"Element found."<<endl;
+    else cout<<"Element not found."<<endl;
+
+    return 0;
+}
