4be1c708a1e49f84abc5e0702c54ad20fd6cb3fc
Unzila Mehtab <rizviunzila0786@gmail.com>
2023-01-10 23:19:48 +0530

    Modified Bubble Sort

--- 6_ModifiedBubbleSort.cpp
+++ 6_ModifiedBubbleSort.cpp
@@ -16,18 +16,21 @@ int main()
 
     for(int i=0;i<n-1;i++)         // For number of passes
     {
+        int flag=0;
         for(int j=0;j<n-1-i;j++)       // For comparison in each pass
         {
             if(A[j]>A[j+1]){
+                flag=1;
                 swap(A[j], A[j+1]);
             }
         }
+        if(flag==0) break;
     }
     cout<<"Array elements after Sorting: ";
     for(int i=0;i<n;i++)
     {
         cout<<A[i]<<" ";
     }
-    
+
     return 0;
 }
